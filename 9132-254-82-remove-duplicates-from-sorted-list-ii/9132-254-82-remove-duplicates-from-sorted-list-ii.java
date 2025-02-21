1df2dda005461d1100ed07b89400a1f840ec7b31
Sayed Iqbal <149720138+sayed-2004@users.noreply.github.com>
2025-02-21 22:33:12 +0000

    Time: 3 ms, Memory: 44.5 MB - LeetHub

--- 9132-254-82-remove-duplicates-from-sorted-list-ii/9132-254-82-remove-duplicates-from-sorted-list-ii.java
+++ 9132-254-82-remove-duplicates-from-sorted-list-ii/9132-254-82-remove-duplicates-from-sorted-list-ii.java
@@ -10,31 +10,6 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        /*ListNode original = head;
-        ArrayList<Integer> list = new ArrayList<>();
-
-        while(original != null){
-            list.add(original.val);
-            original = original.next;
-        }
-
-        for(int i = 0; i < list.size()-1; i++){
-            if(list.get(i) == list.get(i+1)){
-                list.remove(i);
-                list.remove(i+1);
-                i++;
-            }
-        }
-
-        ListNode dummy = new ListNode(0); 
-        ListNode current = dummy; 
-
-        for (int num : list) {  
-            current.next = new ListNode(num); 
-            current = current.next; 
-        }
-
-        return dummy.next;*/
 
         HashMap<Integer, Integer> map = new HashMap<>();
         ListNode original = head;
